30322cd926097f5eaf4f81dcc8ae3c37d692b316
KennethJF23 <150058559+KennethJF23@users.noreply.github.com>
2025-02-02 16:30:56 +0530

    second

--- fashion/src/pages/Home.jsx
+++ fashion/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion,AnimatePresence } from 'framer-motion';
 import {useSnapshot} from 'valtio';
 import threejsLogo from '../public/threejs.png';
+import { CustomButton } from '../components';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -27,6 +28,12 @@ const Home = () => {
             </motion.div>
           <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
         <p className='max-w-md font-normal text-gray-600 text-base'>Create your unique and exclusive shirt with our brand-new 3D customiztion tool <strong>Unleash your imagination</strong>{" "} and define your own style</p>
+          <CustomButton
+            type='filled'
+            title='Customize it'
+            handleClick={()=> state.intro=false}
+            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+          />
           </motion.div>
           </motion.div>
         </motion.section>
