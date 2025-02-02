123e93c5b10d8ee201f8a88f8c5b177207ea67b1
KennethJF23 <150058559+KennethJF23@users.noreply.github.com>
2025-02-02 15:35:41 +0530

    first commit

---
+++ fashion/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { motion,AnimatePresence } from 'framer-motion';
+import {useSnapshot} from 'valtio';
+import threejsLogo from '../public/threejs.png';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+import state from '../store';
+
+const Home = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header>
+            <img src={threejsLogo} alt='logo' className='w-8 h-8 object-contain'/>
+          </motion.header>
+         <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>  
+                <h1 className='head-text'>
+                  LETS'S <br className='xl:block hidden'/> DO IT
+                </h1>
+            </motion.div>
+          <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+        <p className='max-w-md font-normal text-gray-600 text-base'>Create your unique and exclusive shirt with our brand-new 3D customiztion tool <strong>Unleash your imagination</strong>{" "} and define your own style</p>
+          </motion.div>
+          </motion.div>
+        </motion.section>
+
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
