ac5e7eb2b1d225e023a63204d0415cf70845c759
KennethJF23 <150058559+KennethJF23@users.noreply.github.com>
2025-02-06 20:50:03 +0530

    Canvas

---
+++ fashion/src/canvas/CameraRig.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Camerarig = () => {
+  return (
+    <div>
+      CameraRig
+    </div>
+  )
+}
+
+export default Camerarig
--- fashion/src/canvas/index.jsx
+++ fashion/src/canvas/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import {Environment,Center} from '@react-three/fiber'
+import Shirt from './Shirt'
+import BackDrop from './BackDrop'
+import Camerarig from './Camerarig'
 
-const Canvas = () => {
+
+const CanvasModel = () => {
   return (
-    <div>
-      Canvas
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset="city"/>
+
+      <Camerarig>
+        <BackDrop/>
+        <Center>
+          <Shirt/>
+        </Center>
+      </Camerarig>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
